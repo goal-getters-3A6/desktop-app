0539f04bed13f438285fc0b625bd0d914e126215
meryemboukraa <meryem.boukraa@esprit.tn>
2024-02-24 17:38:13 +0100

    UI FIXES

---
+++ src/main/java/edu/esprit/controllers/Modsupprseance.java
@@ -0,0 +1,14 @@
+package edu.esprit.controllers;
+
+import javafx.event.ActionEvent;
+
+public class Modsupprseance {
+    public void browseImage(ActionEvent actionEvent) {
+    }
+
+    public void modifierseance(ActionEvent actionEvent) {
+    }
+
+    public void supprimerseance(ActionEvent actionEvent) {
+    }
+}
